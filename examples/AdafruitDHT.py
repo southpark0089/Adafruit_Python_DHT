590c777daa0fead921de625adcd66678946ac621
southpark0089 <74525626+southpark0089@users.noreply.github.com>
2020-11-24 23:55:16 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -29,8 +29,8 @@ import Adafruit_DHT
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-deviceId = 'DNVEDCFZ'
-deviceKey = '489vJVLK2yn4fXfD' 
+deviceId = 'DosLM0A7'
+deviceKey = 'LosznezRaR5PJQW0' 
 
 def post_to_mcs(payload): 
 	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
