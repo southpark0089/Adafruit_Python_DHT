1a69e67082657ee4975b38d0941cab05932a443c
southpark0089 <74525626+southpark0089@users.noreply.github.com>
2020-11-25 01:37:41 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -10,7 +10,6 @@
 
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-#た焼きは一番おいしですね！
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
